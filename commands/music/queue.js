3c5cb2d0da9db21f2fe65bd1985efc133a5f5f1a
yunggflamee <infinityabs4@gmail.com>
2025-04-21 18:00:12 +0530

    ws

--- commands/music/queue.js
+++
@@ -1,6 +0,0 @@
-const { createAudioPlayer } = require('@discordjs/voice');
-
-const queue = new Map(); // guildId => { connection, songs: [] }
-const player = createAudioPlayer();
-
-module.exports = { queue, player };
